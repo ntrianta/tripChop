7c516daf78c1c3c245d2bd02f6476b0767caf975
Nick Triantafyllidis <ntrianta90@gmail.com>
2015-06-16 17:13:16 +0200

    flow aggregation

--- app/controllers/flow.go
+++ app/controllers/flow.go
@@ -1,12 +1,43 @@
 package controllers
 
-import "github.com/revel/revel"
+import (
+	"github.com/ntrianta/tripChop/app/models"
+	"github.com/revel/revel"
+)
+
+type FlowJson struct {
+	SourceIP   string  `json:sourceIP`
+	DestIP     string  `json:destinationIP`
+	SourcePort string  `json:sourcePort`
+	DestPort   string  `json:destinationPort`
+	Proto      string  `json:protocol`
+	Bytes      float64 `json:bytes`
+	Avg        float64 `json:average`
+	Packets    int     `json:packets`
+}
 
 type Flow struct {
 	*revel.Controller
 }
 
-func (c Flow) Foo() revel.Result {
-  bar := "meh meh meh"
-	return c.Render(bar)
+func (c Flow) AggregateFlow() revel.Result {
+	return c.Render()
+}
+
+func (c Flow) FlowJson() revel.Result {
+	results := models.Aggregate()
+	output := []*FlowJson{}
+	for _, r := range results {
+		output = append(output, &FlowJson{
+			SourceIP:   r.Flow["si"].(string),
+			DestIP:     r.Flow["di"].(string),
+			SourcePort: r.Flow["sp"].(string),
+			DestPort:   r.Flow["dp"].(string),
+			Proto:      r.Flow["p"].(string),
+			Bytes:      r.Bytes,
+			Packets:    r.Packets,
+			Avg:        r.Average,
+		})
+	}
+	return c.RenderJson(output)
 }
---
+++ app/models/flow.go
@@ -0,0 +1,36 @@
+package models
+
+import (
+	//"github.com/revel/revel"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"time"
+)
+
+type Flow struct {
+	Flow    bson.M  `bson:"_id"`
+	Packets int     `bson:"p"`
+	Bytes   float64 `bson:"b"`
+	Average float64 `bson:"a"`
+}
+
+func Aggregate() []Flow {
+	results := []Flow{}
+
+	fromDate := time.Date(2014, time.August, 12, 9, 0, 0, 0, time.UTC)
+	toDate := time.Date(2014, time.August, 12, 10, 0, 0, 0, time.UTC)
+
+	session, _ := mgo.Dial("localhost:27017")
+	collection := session.DB("pcap").C("flow")
+	collection.Pipe([]bson.M{
+		{"$match": bson.M{"ts": bson.M{"$gte": fromDate, "$lte": toDate}, "p": "TCP", "sp": bson.M{"$ne": "ignore"}}},
+		{"$group": bson.M{
+			"_id": bson.M{"si": "$si", "di": "$di", "sp": "$sp", "dp": "$dp", "p": "$p"},
+			"p":   bson.M{"$sum": 1},
+			"b":   bson.M{"$sum": "$l"},
+			"a":   bson.M{"$avg": "$l"},
+		}},
+	}).All(&results)
+	session.Close()
+	return results
+}
